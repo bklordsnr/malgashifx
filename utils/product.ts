bad315c2c28bbe6f4edd8273cb634eed32ab3fb5
lambkigen <lordkigen@gmail.com>
2026-04-28 20:18:39 +0300

    cloud 12

--- utils/product.ts
+++ utils/product.ts
@@ -1,19 +1,5 @@
 export const products = [
   {
-    id: "65bf49743eda8f76799a9578",
-    name: "Basic Package",
-    description:
-      "Principal return on maturity, instant withdrawal to your mobile money or bank profession chart and 24/7 support. package maturity time 24-25 hours maximum",
-    price: "50",
-    profit: "248",
-    category: "Student Plan",
-    availability: true,
-    personalAccount: true,
-    swapdiscount: false,
-    customizedinvestment: false,
-    color: "green",
-  },
-  {
     id: "65bf49743eda8f76799a9539",
     name: "Basic Package",
     description:
